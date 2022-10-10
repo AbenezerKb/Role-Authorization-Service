3e68585e78f0c634040a6fca51c471d789ed98f1
yonas kemon <yonaskemon01@gmail.com>
2022-10-10 09:45:22 +0300

    AUT-2 initiated persistance for service

--- initiator/persistence.go
+++ initiator/persistence.go
@@ -4,15 +4,18 @@ import (
 	"2f-authorization/internal/constants/dbinstance"
 	"2f-authorization/internal/storage"
 	"2f-authorization/internal/storage/opa"
+	"2f-authorization/internal/storage/service"
 	"2f-authorization/platform/logger"
 )
 
 type Persistence struct {
-	opa storage.Policy
+	opa     storage.Policy
+	service storage.Service
 }
 
 func InitPersistence(db dbinstance.DBInstance, log logger.Logger) Persistence {
 	return Persistence{
-		opa: opa.Init(db, log),
+		opa:     opa.Init(db, log),
+		service: service.Init(db, log),
 	}
 }
