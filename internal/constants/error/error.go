390a2b17a7bcecbc1e901236ef27b2e295a80476
yonas kemon <yonaskemon01@gmail.com>
2022-10-10 09:47:58 +0300

    created new errorx types

--- internal/constants/error/error.go
+++ internal/constants/error/error.go
@@ -24,15 +24,38 @@ var Error = []ErrorType{
 		ErrorCode: http.StatusInternalServerError,
 		ErrorType: ErrOpaPrepareEvalError,
 	},
+	{
+		ErrorCode: http.StatusBadRequest,
+		ErrorType: ErrInvalidUserInput,
+	},
+	{
+		ErrorCode: http.StatusBadRequest,
+		ErrorType: ErrDataExists,
+	},
+	{
+		ErrorCode: http.StatusInternalServerError,
+		ErrorType: ErrReadError,
+	},
+	{
+		ErrorCode: http.StatusInternalServerError,
+		ErrorType: ErrWriteError,
+	},
 }
 
 var (
-	opa = errorx.NewNamespace("opa error")
-	db  = errorx.NewNamespace("db error")
+	opa          = errorx.NewNamespace("opa error")
+	db           = errorx.NewNamespace("db error")
+	invalidInput = errorx.NewNamespace("validation error").ApplyModifiers(errorx.TypeModifierOmitStackTrace)
+	dbError      = errorx.NewNamespace("db error")
+	duplicate    = errorx.NewNamespace("duplicate").ApplyModifiers(errorx.TypeModifierOmitStackTrace)
 )
 
 var (
 	ErrOpaEvalError         = errorx.NewType(opa, "error evaluating")
 	ErrOpaUpdatePolicyError = errorx.NewType(db, "error updating policy data")
 	ErrOpaPrepareEvalError  = errorx.NewType(opa, "error preparing for evaluation")
+	ErrInvalidUserInput     = errorx.NewType(invalidInput, "invalid user input")
+	ErrWriteError           = errorx.NewType(dbError, "could not write to db")
+	ErrReadError            = errorx.NewType(dbError, "could not read data from db")
+	ErrDataExists           = errorx.NewType(duplicate, "data already exists")
 )
