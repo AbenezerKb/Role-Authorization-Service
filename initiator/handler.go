3313ff3ec320e9cf25e20c0870c5769c0ac9ff1b
yonas kemon <yonaskemon01@gmail.com>
2022-10-10 09:46:09 +0300

    AUT-2 initiated service layer

--- initiator/handler.go
+++ initiator/handler.go
@@ -1,12 +1,17 @@
 package initiator
 
 import (
+	"2f-authorization/internal/handler/rest"
+	"2f-authorization/internal/handler/rest/service"
 	"2f-authorization/platform/logger"
 )
 
 type Handler struct {
+	service rest.Service
 }
 
 func InitHandler(module Module, log logger.Logger) Handler {
-	return Handler{}
+	return Handler{
+		service: service.Init(log, module.service),
+	}
 }
--- initiator/module.go
+++ initiator/module.go
@@ -1,13 +1,18 @@
 package initiator
 
 import (
+	"2f-authorization/internal/module"
+	"2f-authorization/internal/module/service"
 	"2f-authorization/platform/logger"
 	opa_platform "2f-authorization/platform/opa"
 )
 
 type Module struct {
+	service module.Service
 }
 
 func InitModule(persistence Persistence, log logger.Logger, opa opa_platform.Opa) Module {
-	return Module{}
+	return Module{
+		service:  service.Init(log,persistence.service,opa),
+	}
 }
