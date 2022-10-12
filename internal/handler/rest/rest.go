51d09c46d6ce18b45545cf043481d3337dd41fd8
yonas kemon <yonaskemon01@gmail.com>
2022-10-12 12:19:04 +0300

    AUT-3 created delete service handler

--- internal/handler/rest/rest.go
+++ internal/handler/rest/rest.go
@@ -4,4 +4,5 @@ import "github.com/gin-gonic/gin"
 
 type Service interface {
 	CreateService(ctx *gin.Context)
+	DeletService(ctx *gin.Context)
 }
--- internal/handler/rest/service/service.go
+++ internal/handler/rest/service/service.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
@@ -42,4 +43,22 @@ func (s *service) CreateService(ctx *gin.Context) {
 	}
 
 	constants.SuccessResponse(ctx, http.StatusCreated, createdService, nil)
+}
+
+func (s *service) DeletService(ctx *gin.Context) {
+
+	serviceId, err := uuid.Parse(ctx.GetString("x-service-id"))
+	if err != nil {
+		err := errors.ErrInvalidUserInput.New("invalid input")
+		s.logger.Info(ctx, "invalid input", zap.Error(err))
+		_ = ctx.Error(err)
+		return
+	}
+
+	if err := s.serviceModule.DeleteService(ctx, dto.Service{ID: serviceId}); err != nil {
+		_ = ctx.Error(err)
+		return
+	}
+
+	constants.SuccessResponse(ctx, http.StatusOK, nil, nil)
 }
