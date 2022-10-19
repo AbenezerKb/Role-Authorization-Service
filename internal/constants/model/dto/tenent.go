0c1f8a85e175fa269986c169a0a56d2572aafea6
yonas kemon <yonaskemon01@gmail.com>
2022-10-19 22:37:46 +0300

    updated tenants dto

--- internal/constants/model/dto/tenent.go
+++ internal/constants/model/dto/tenent.go
@@ -23,12 +23,14 @@ type CreateTenent struct {
 	TenantName string `json:"tenant_name"`
 	//ServiceID  is the service id of service.
 	ServiceID uuid.UUID `json:"service_id"`
+	DomainID  uuid.UUID `json:"domain_id"`
 }
 
 func (d CreateTenent) Validate() error {
+
 	return validation.ValidateStruct(&d,
 		validation.Field(&d.TenantName, validation.Required.Error("tenant name can not be blank")),
 		validation.Field(&d.ServiceID, validation.Required.Error("service id is required"), is.UUID),
+		validation.Field(&d.DomainID, validation.NotIn(uuid.Nil.String()).Error("domain id is required")),
 	)
-
 }
