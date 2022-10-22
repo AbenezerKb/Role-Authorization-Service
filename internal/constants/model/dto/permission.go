8224e38b9269a593500c3173cb7852e244c78139
yonas kemon <yonaskemon01@gmail.com>
2022-10-22 12:55:59 +0300

    AUT-32 added dto for permission

--- internal/constants/model/dto/permission.go
+++ internal/constants/model/dto/permission.go
@@ -2,10 +2,35 @@ package dto
 
 import (
 	"encoding/json"
+	"time"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/google/uuid"
 )
+
+type Permission struct {
+	// ID is the unique identifier for the service.
+	// It is automatically generated when the permission is registered.
+	ID uuid.UUID `json:"id"`
+	// Name is the name of the permission being created
+	Name string `json:"name,omitempty"`
+	// Description is the description of the permission being created
+	Description string `json:"description,omitempty"`
+	// Statement is an object that holds the action, resource and effect of the permission being created
+	Statement `json:"statement,omitempty"`
+	// ServiceID is the id of the service the permission belongs to
+	ServiceID uuid.UUID `json:"service_id,omitempty"`
+	// Domain is an array that holds the id of the domains the permission is accessible at
+	Domain []string `json:"domains,omitempty"`
+	// Status is the status of the permission.
+	Status string `json:"status,omitempty"`
+	// DeletedAt is the time this permission was deleted.
+	DeletedAt time.Time `json:"-"`
+	// CreatedAt is the time this permission was created.
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	// UpdatedAt is the time this permission was last updated.
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
 
 type CreatePermission struct {
 	// Name is the name of the permission being created
@@ -13,7 +38,7 @@ type CreatePermission struct {
 	// Description is the description of the permission being created
 	Description string `json:"description"`
 	// Statement is an object that holds the action, resource and effect of the permission being created
-	Statement `json:"statement"`
+	Statement Statement `json:"statement"`
 	// ServiceID is the id of the service the permission belongs to
 	ServiceID uuid.UUID `json:"service_id"`
 	// Domain is an array that holds the id of the domains the permission is accessible at
@@ -27,7 +52,7 @@ type Statement struct {
 	// Resource is the urn for the path that is being accessed
 	Resource string `json:"resource"`
 	// Action is the urn for the action(method) the user is taking on the resource
-	Action   string `json:"action"`
+	Action string `json:"action"`
 }
 
 func (a Statement) Value() ([]byte, error) {
@@ -48,4 +73,17 @@ func (s Statement) Validate() error {
 		validation.Field(&s.Effect, validation.Required.Error("statement effect is required")),
 		validation.Field(&s.Action, validation.Required.Error("statement action is required")),
 	)
+}
+
+type GetAllPermissionsReq struct {
+	//TenantName is the name of the tenant
+	TenantName string `json:"tenant_name"`
+	//ServiceID  is the service id of service.
+	ServiceID uuid.UUID `json:"service_id"`
+}
+
+func (g GetAllPermissionsReq) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.TenantName, validation.Required.Error("tenant name can not be blank")),
+		validation.Field(&g.ServiceID, validation.NotIn(uuid.Nil.String()).Error("service id is required")))
 }
--- internal/constants/model/dto/tenent.go
+++ internal/constants/model/dto/tenent.go
@@ -1,29 +1,18 @@
 package dto
 
 import (
-	"time"
-
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/google/uuid"
 )
-
-type TenantResponse struct {
-	ID         uuid.UUID `json:"id"`
-	Status     bool      `json:"status"`
-	TenantName string    `json:"tenant_name"`
-	ServiceID  uuid.UUID `json:"service_id"`
-	DeletedAt  time.Time `json:"deleted_at"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
-}
 
 type CreateTenent struct {
 	//TenantName is the name of the tenant
 	TenantName string `json:"tenant_name"`
 	//ServiceID  is the service id of service.
 	ServiceID uuid.UUID `json:"service_id"`
-	DomainID  uuid.UUID `json:"domain_id"`
+	// DomainID is the id of the domain the tenant is in.
+	DomainID uuid.UUID `json:"domain_id"`
 }
 
 func (d CreateTenent) Validate() error {
