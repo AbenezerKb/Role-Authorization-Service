ea92b7c392fbf1db3eb2d2110f2b616c14228a20
yonas kemon <yonaskemon01@gmail.com>
2022-11-02 09:46:59 +0300

    AUT-36 generated sql functions for register tenant permission

--- internal/constants/model/db/tenent.sql.go
+++ internal/constants/model/db/tenent.sql.go
@@ -7,8 +7,10 @@ package db
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgtype"
 )
 
 const createTenent = `-- name: CreateTenent :exec
@@ -56,6 +58,68 @@ func (q *Queries) GetTenentWithNameAndServiceId(ctx context.Context, arg GetTene
 		&i.UpdatedAt,
 		&i.DomainID,
 		&i.Inherit,
+	)
+	return i, err
+}
+
+const isPermissionExistsInTenant = `-- name: IsPermissionExistsInTenant :one
+SELECT count_rows() FROM permissions p join tenants t on p.tenant_id=t.id WHERE p.name =$1 and p.service_id=$2 and t.tenant_name=$3 and p.deleted_at IS NULL and p.deleted_at IS NULL
+`
+
+type IsPermissionExistsInTenantParams struct {
+	Name       string    `json:"name"`
+	ServiceID  uuid.UUID `json:"service_id"`
+	TenantName string    `json:"tenant_name"`
+}
+
+func (q *Queries) IsPermissionExistsInTenant(ctx context.Context, arg IsPermissionExistsInTenantParams) (interface{}, error) {
+	row := q.db.QueryRow(ctx, isPermissionExistsInTenant, arg.Name, arg.ServiceID, arg.TenantName)
+	var count_rows interface{}
+	err := row.Scan(&count_rows)
+	return count_rows, err
+}
+
+const tenantRegisterPermission = `-- name: TenantRegisterPermission :one
+INSERT INTO permissions (name,description,statement,service_id,tenant_id)
+SELECT $1,$2,$3,$4,t.id from tenants t where t.tenant_name=$5 and t.deleted_at is null
+RETURNING permissions.id,permissions.statement,permissions.description,permissions.name,permissions.created_at,permissions.service_id, $5::string as tenant
+`
+
+type TenantRegisterPermissionParams struct {
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Statement   pgtype.JSON `json:"statement"`
+	ServiceID   uuid.UUID   `json:"service_id"`
+	TenantName  string      `json:"tenant_name"`
+}
+
+type TenantRegisterPermissionRow struct {
+	ID          uuid.UUID   `json:"id"`
+	Statement   pgtype.JSON `json:"statement"`
+	Description string      `json:"description"`
+	Name        string      `json:"name"`
+	CreatedAt   time.Time   `json:"created_at"`
+	ServiceID   uuid.UUID   `json:"service_id"`
+	Tenant      string      `json:"tenant"`
+}
+
+func (q *Queries) TenantRegisterPermission(ctx context.Context, arg TenantRegisterPermissionParams) (TenantRegisterPermissionRow, error) {
+	row := q.db.QueryRow(ctx, tenantRegisterPermission,
+		arg.Name,
+		arg.Description,
+		arg.Statement,
+		arg.ServiceID,
+		arg.TenantName,
+	)
+	var i TenantRegisterPermissionRow
+	err := row.Scan(
+		&i.ID,
+		&i.Statement,
+		&i.Description,
+		&i.Name,
+		&i.CreatedAt,
+		&i.ServiceID,
+		&i.Tenant,
 	)
 	return i, err
 }
--- internal/constants/query/queries/tenent.sql
+++ internal/constants/query/queries/tenent.sql
@@ -12,3 +12,10 @@ service_id
 SELECT * FROM tenants WHERE 
 tenant_name = $1 AND service_id = $2 AND deleted_at IS NULL;
 
+-- name: TenantRegisterPermission :one
+INSERT INTO permissions (name,description,statement,service_id,tenant_id)
+SELECT $1,$2,$3,$4,t.id from tenants t where t.tenant_name=$5 and t.deleted_at is null
+RETURNING permissions.id,permissions.statement,permissions.description,permissions.name,permissions.created_at,permissions.service_id, $5::string as tenant;
+
+-- name: IsPermissionExistsInTenant :one
+SELECT count_rows() FROM permissions p join tenants t on p.tenant_id=t.id WHERE p.name =$1 and p.service_id=$2 and t.tenant_name=$3 and p.deleted_at IS NULL and p.deleted_at IS NULL ;
