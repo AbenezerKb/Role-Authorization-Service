b4a2d312eab1be6555c4203f0da3436294a2b9e7
yonas kemon <yonaskemon01@gmail.com>
2022-11-08 09:22:41 +0300

    AUT-32 generated sql query to get permissions

--- internal/constants/dbinstance/permissions.sql.go
+++ internal/constants/dbinstance/permissions.sql.go
@@ -11,9 +11,9 @@ const listPermissions = `-- name: ListPermissions :many
 with _tenant as (
     select tenants.domain_id,tenants.id,tenants.inherit from tenants where tenant_name =$1 and tenants.service_id=$2 and deleted_at IS NULL
 )
-select p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id  from _tenant,permissions p  join permission_domains pd on p.id = pd.permission_id where pd.domain_id = _tenant.domain_id and _tenant.inherit = true
+select p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id,coalesce(json_agg(json_build_object('name',p2.name,'status',p2.status,'description',p2.description,'statement',p2.statement,'created_at',p2.created_at,'service_id',p2.service_id,'id',p2.id)) filter ( where p2.name is not null ),'[]') AS inherited_permissions from _tenant,permissions p  left join permission_domains pd on p.id = pd.permission_id left join permissions_hierarchy ph on p.id = ph.parent join permissions p2 on p2.id = ph.child where p2.deleted_at is null and pd.domain_id = _tenant.domain_id and _tenant.inherit = true group by p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id
 UNION
-select p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id  from permissions p,_tenant where p.tenant_id =_tenant.id and deleted_at IS NULL
+select p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id,coalesce(json_agg(json_build_object('name',p2.name,'status',p2.status,'description',p2.description,'statement',p2.statement,'created_at',p2.created_at,'service_id',p2.service_id,'id',p2.id))filter ( where p2.name is not null ),'[]') AS inherited_permissions  from permissions p left join permissions_hierarchy ph on p.id = ph.parent left join permissions p2 on p2.id = ph.child,_tenant where  p2.deleted_at is null and p.tenant_id =_tenant.id and p.deleted_at IS NULL group by p.name,p.status,p.description,p.statement,p.created_at,p.service_id,p.id
 `
 
 type ListPermissionsParams struct {
@@ -38,6 +38,7 @@ func (q *DBInstance) ListPermissions(ctx context.Context, arg ListPermissionsPar
 			&i.CreatedAt,
 			&i.ServiceID,
 			&i.ID,
+			&i.InheritedPermissions,
 		); err != nil {
 			return nil, err
 		}
