c16bb4592c28ffb4c085008e4bbed94ea9162915
yonas kemon <yonaskemon01@gmail.com>
2022-12-14 15:11:49 +0300

    AUT-43 added a dto for update tenant status

--- internal/constants/model/dto/tenent.go
+++ internal/constants/model/dto/tenent.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"2f-authorization/internal/constants"
+
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/google/uuid"
@@ -21,5 +23,16 @@ func (d CreateTenent) Validate() error {
 		validation.Field(&d.TenantName, validation.Required.Error("tenant name can not be blank")),
 		validation.Field(&d.ServiceID, validation.Required.Error("service id is required"), is.UUID),
 		validation.Field(&d.DomainID, validation.NotIn(uuid.Nil.String()).Error("domain id is required")),
+	)
+}
+
+type UpdateTenantStatus struct {
+	// Status is new status of the tenant that is going to replace the old one
+	Status string `json:"status"`
+}
+
+func (u UpdateTenantStatus) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Status, validation.Required.Error("status is required"), validation.In(constants.Active, constants.InActive).Error("invalid status")),
 	)
 }
