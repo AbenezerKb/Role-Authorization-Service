79b59eeb332ca0d76b1e58ab794178dfb4546b41
alazarbeyene <alazarbeyene99@gmail.com>
2023-03-13 05:03:07 -0400

    AUT-49 update opa file to evaluate using the opa server

--- initiator/opa.go
+++ initiator/opa.go
@@ -4,20 +4,22 @@ import (
 	"2f-authorization/platform/logger"
 	opa_platform "2f-authorization/platform/opa"
 	"context"
+	"fmt"
 	"os"
 
 	"go.uber.org/zap"
 )
 
-func InitOpa(ctx context.Context, path string, persistence Persistence, log logger.Logger) opa_platform.Opa {
-	policy, err := os.ReadFile(path)
+func InitOpa(ctx context.Context, rego, data, server string, persistence Persistence, log logger.Logger) opa_platform.Opa {
+	policy, err := os.ReadFile(rego)
 	if err != nil {
 		log.Fatal(ctx, "error reading the policy file")
 	}
-
-	op := opa_platform.Init(string(policy), persistence.opa, log)
+	fmt.Println("file path", data)
+	op := opa_platform.Init(string(policy), persistence.opa, data, rego, server, log)
 	if err := op.Refresh(ctx, "initiate"); err != nil {
 		log.Fatal(ctx, "error getting opa data", zap.Error(err))
 	}
+
 	return op
 }
--- platform/opa/opa.go
+++ platform/opa/opa.go
@@ -4,14 +4,21 @@ import (
 	errors "2f-authorization/internal/constants/error"
 	"2f-authorization/internal/constants/model"
 	"2f-authorization/platform/logger"
+	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
 
 	dbstore "2f-authorization/internal/storage"
 
+	"github.com/goccy/go-json"
 	"github.com/open-policy-agent/opa/rego"
 	"github.com/open-policy-agent/opa/storage"
-	"github.com/open-policy-agent/opa/storage/inmem"
 	"github.com/open-policy-agent/opa/util"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
@@ -23,41 +30,73 @@ type Opa interface {
 }
 
 type opa struct {
-	db     dbstore.Policy
-	store  storage.Store
-	policy string
-	Query  string
-	log    logger.Logger
-	query  rego.PreparedEvalQuery
+	db       dbstore.Policy
+	store    storage.Store
+	policy   string
+	Query    string
+	log      logger.Logger
+	filepath string
+	regopath string
+	server   string
+	query    rego.PreparedEvalQuery
 }
 
-func Init(policy string, policyDb dbstore.Policy, log logger.Logger) Opa {
-	return &opa{
-		policy: policy,
-		db:     policyDb,
-		log:    log,
+func Init(policy string, policyDb dbstore.Policy, filepath, regopath, server string, log logger.Logger) Opa {
+	output, _ := exec.Command("lsof", "-t", "-i", ":8181").Output()
+	if len(output) != 0 {
+		if err := exec.Command("killall", "opa").Run(); err != nil {
+			err := errors.ErrOpaPrepareEvalError.Wrap(err, "error  while cleaning used port")
+			log.Error(context.Background(), "error  while cleaning used port", zap.Error(err))
+		}
+
 	}
+
+	go func() {
+
+		err := exec.Command(server, "run", "--server", "--watch", regopath, filepath).Run()
+		if err != nil {
+			err := errors.ErrOpaPrepareEvalError.Wrap(err, "error  Initializing OPA  Server")
+			log.Error(context.Background(), "error preparing the rego for eval", zap.Error(err))
+		}
+	}()
+
+	return &opa{
+		policy:   policy,
+		db:       policyDb,
+		filepath: filepath,
+		regopath: regopath,
+		server:   server,
+		log:      log,
+	}
+}
+
+type responseBody struct {
+	Response bool `json:"result"`
+}
+type RequestBody struct {
+	Input model.Request `json:"input"`
 }
 
 func (o *opa) Allow(ctx context.Context, req model.Request) (bool, error) {
-
-	input := map[string]interface{}{
-		"subject":  req.Subject,
-		"resource": req.Resource,
-		"action":   req.Action,
-		"service":  req.Service,
-		"tenant":   req.Tenant,
-		"fields":   req.Fields,
+	posturl := viper.GetString("opa.server_addr")
+	reqst := RequestBody{
+		Input: req,
 	}
+	resp := responseBody{}
+	js, _ := json.Marshal(reqst)
 
-	results, err := o.query.Eval(ctx, rego.EvalInput(input))
+	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(js))
 	if err != nil {
-		err := errors.ErrOpaEvalError.Wrap(err, "can not evaluate the user")
-		o.log.Error(ctx, "error evaluating the user", zap.Error(err), zap.Any("input", input))
+		err := errors.ErrOpaPrepareEvalError.Wrap(err, "error while preparing evaluation")
+		o.log.Error(ctx, "error preparing the opa data", zap.Error(err))
 		return false, err
 	}
-
-	return results.Allowed(), nil
+	httpCli := &http.Client{}
+	res, _ := httpCli.Do(r)
+	defer res.Body.Close()
+	json.NewDecoder(res.Body).Decode(&resp)
+	fmt.Println(resp.Response)
+	return resp.Response, nil
 }
 
 func (o *opa) Refresh(ctx context.Context, reason string) error {
@@ -66,20 +105,6 @@ func (o *opa) Refresh(ctx context.Context, reason string) error {
 		return err
 	}
 
-	qr, err := rego.New(
-		rego.Query("data.authz.allow"),
-		rego.Store(o.store),
-		rego.Module("authz.rego", o.policy),
-	).PrepareForEval(ctx)
-
-	if err != nil {
-		err := errors.ErrOpaPrepareEvalError.Wrap(err, "error preparing for evaluation")
-		o.log.Error(ctx, "error preparing the rego for eval", zap.Error(err))
-		return err
-	}
-
-	o.query = qr
-	o.log.Info(ctx, "successfully triggered policy data update")
 	return nil
 }
 
@@ -89,10 +114,39 @@ func (o *opa) GetData(ctx context.Context) error {
 		return err
 	}
 	var services map[string]interface{}
-	_ = util.UnmarshalJSON(data, &services)
-	o.store = inmem.NewFromObject(map[string]interface{}{
+	err = util.UnmarshalJSON(data, &services)
+	if err != nil {
+		err := errors.ErrOpaUpdatePolicyError.Wrap(err, "error while preparing opa data to json")
+		o.log.Error(ctx, "error while updating  opa data", zap.Error(err))
+		return err
+	}
+
+	serv := map[string]interface{}{
 		"services": services,
-	})
+	}
+	servicedata, err := json.Marshal(serv)
+	if err != nil {
+		err := errors.ErrOpaUpdatePolicyError.Wrap(err, "error while preparing opa service data to json")
+		o.log.Error(ctx, "error while updating  opa service data", zap.Error(err))
+		return err
+	}
+
+	f, err := os.OpenFile(o.filepath,
+		os.O_WRONLY|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		err := errors.ErrOpaUpdatePolicyError.Wrap(err, "can not read  opa data")
+		o.log.Error(ctx, "error while reading opa data", zap.Error(err))
+		return err
+	}
+
+	defer f.Close()
+	if _, err := f.WriteString(string(servicedata)); err != nil {
+		err := errors.ErrOpaUpdatePolicyError.Wrap(err, "can not write new opa data")
+		o.log.Error(ctx, "error while updating opa data", zap.Error(err))
+		return err
+
+	}
+	time.Sleep(time.Millisecond * 20)
 	return nil
 }
 
